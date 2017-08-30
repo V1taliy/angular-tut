b24d5ac0ce77c76f81547129bdaf7309353c2d20
vsakhno <biggone24@gmail.com>
2017-08-30 18:05:00 +0300

    first commit22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 export class Hero {
   id: number;
   name: string;
-
-
 }
 
 @Component({
@@ -29,3 +27,16 @@ export class AppComponent  {
 };
 }
 
+const HEROES: Hero[] = [
+  {id: 11, name: 'Mr. Nice'},
+  {id: 12, name: 'Narco'},
+  {id: 13, name: 'Ololo'},
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+]
+//heroes = HEROES;
